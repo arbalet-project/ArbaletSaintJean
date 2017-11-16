1162b39b359476c4dddf15e94cd778ef0c344f3a
Aymeric Devriesere <adevriesere@info-gandinix.iut.bx1>
2017-11-16 16:00:47 +0100

    Sequenceur a la fin de la seance ndu 16/1/2017

--- examples/sequencer/sequencer.ino
+++ examples/sequencer/sequencer.ino
@@ -8,6 +8,7 @@
    Toutes les animations (classes filles) vont hériter de cette classe mère. */ 
 #include <Animation.h>
 
+
 /* Pour faire simple on inclue tous les ficheirs de code (directement les .cpp) des animations ici */
 #include <animations/animation_vide/animation_vide.cpp>
 
@@ -19,8 +20,19 @@
 **************/
 uint8_t WIDTH=20;   // Largeur en nombre de pixels
 uint8_t HEIGHT=15;  // Hauteur en nombre de pixels
-uint8_t* PIXELS;    // Les couleurs RGB ou HSV de chaque pixel (attention : mémoire à allouer au démarrage)
+uint8_t*** PIXELS;    // Les couleurs RGB ou HSV de chaque pixel (attention : mémoire à allouer au démarrage)
 
+Animation *Animations[12];
+
+void reset(uint8_t*** tab){
+  for(int i = 0; i < WIDTH; i++){
+    for(int j = 0; j < HEIGHT; j++){
+      for(int c = 0; c < 3; c++){
+        tab[i][j][c] = 255;
+      }
+    }
+  }
+}
 
 /*************
     Point d'entrée du séquenceur
@@ -31,13 +43,19 @@ void setup() {
     // TODO : allouer la mémoire des pixels, instancier la bilbiothèque Adafruit, déclarer les animations, appeler leur setup ...
     
     // Aide à la prise en main des pointeurs ci-dessous, code à supprimer, mettre dans un tableau de pointeurs sur des animations, etc
+    PIXELS = (uint8_t***)malloc(20*15*3);
+    reset(PIXELS);
     AnimationVide *animation_vide = new AnimationVide();   // Déclaration de l'animation et allocation de la mémoire avec le mot-clé "new" (allocation obligatoire avec un pointeur)
-    int retour = animation_vide->setup(15, 20, 100);  // Appel du setup() conformément à votre interface (peut-etre à faire évoluer)
-    animation_vide->loop();  // Exécution d'une loop
+    for(int i = 0; i < 1; i++){
+      Animations[i] -> setup(15,20,100);
+    }
 }
 
 
 void loop() {
     // C'est le loop() du séquenceur, aucun code d'animation ne va ici
     // TODO : appeler le loop de l'animation courante, ou bien changer d'animation
+    for( int i = 0; i < 1; i++){
+      Animations[i]->loop();
+    }
 }
