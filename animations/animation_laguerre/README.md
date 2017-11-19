4de5ba595c93988d50258babf691d686f5bbddec
Dori4ne <dory.laguerre@gmail.com>
2017-11-19 17:19:35 +0100

    Ajout du fantôme

--- animations/animation_laguerre/README.md
+++ animations/animation_laguerre/README.md
@@ -3,8 +3,7 @@
 Ceci est la documentation de mon animation, je décris dans ce document au format [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) ce qu'elle fait et comment elle marche.
 
 ## A quoi ressemble mon animation
-Pacman animé (TODO : Faire avancer et rajouter fantome)
-
+Pacman animé poursuivi par un fantôme (TODO : fantôme tristepoursuivi par pac man)
 ## Comment fonctionne son code
 Dessin de tableau de booleens a chaque tour de boucle
 
--- animations/animation_laguerre/animation_laguerre.cpp
+++ animations/animation_laguerre/animation_laguerre.cpp
@@ -56,6 +56,54 @@ class AnimationLaguerre : public Animation {
 	        {0,0,0,0,1,1,1,1,1,0,0,0,0}
 		};
 
+        int ghostOne[13][14] = {
+            {0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+            {0,0,0,1,1,1,1,1,1,1,1,0,0,0},
+            {0,0,1,1,1,1,1,1,1,1,1,1,0,0},
+            {0,1,1,1,2,2,1,1,1,1,2,2,1,0},
+            {0,1,1,2,2,2,2,1,1,2,2,2,2,0},
+            {0,1,1,2,2,3,3,1,1,2,2,3,3,0},
+            {1,1,1,2,2,3,3,1,1,2,2,3,3,1},
+            {1,1,1,1,2,2,1,1,1,1,2,2,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,0,1,1,1,0,0,1,1,1,0,1,1},
+            {1,0,0,0,1,1,0,0,1,1,0,0,0,1}
+        };
+
+        int ghostTwo[13][14] = {
+            {0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+            {0,0,0,1,1,1,1,1,1,1,1,0,0,0},
+            {0,0,1,1,1,1,1,1,1,1,1,1,0,0},
+            {0,1,1,1,2,2,1,1,1,1,2,2,1,0},
+            {0,1,1,2,2,2,2,1,1,2,2,2,2,0},
+            {0,1,1,2,2,3,3,1,1,2,2,3,3,0},
+            {1,1,1,2,2,3,3,1,1,2,2,3,3,1},
+            {1,1,1,1,2,2,1,1,1,1,2,2,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,0,1,1,1,1,0,1,1,1,1},
+            {0,1,1,0,0,0,1,1,0,0,0,1,1,0}
+        };
+
+        int ghostDead[13][14] = {
+            {0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+            {0,0,0,1,1,1,1,1,1,1,1,0,0,0},
+            {0,0,1,1,1,1,1,1,1,1,1,1,0,0},
+            {0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,2,2,1,1,2,2,1,1,1,0},
+            {1,1,1,1,2,2,1,1,2,2,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,2,2,1,1,2,2,1,1,2,2,1,1},
+            {1,2,1,1,2,2,1,1,2,2,1,1,2,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,0,1,1,1,0,0,1,1,1,0,1,1},
+            {1,0,0,0,1,1,0,0,1,1,0,0,0,1}
+        };
+
         AnimationLaguerre() {
             // Le constructeur. Si j'ai des valeurs par défaut à donner aux variables membres, c'est ici
         }
@@ -72,9 +120,17 @@ class AnimationLaguerre : public Animation {
 	    
 	        for(int posX = 0 ; posX < WIDTH ; posX++){
                 drawPacMan(pacManClosed,   posX);
-                drawPacMan(pacManMidOpen,  posX);	
+                drawGhost(ghostOne,        posX);
+                //delay(125);                
+                drawPacMan(pacManMidOpen,  posX);
+                drawGhost(ghostTwo,        posX);	
+                //delay(125);
                 drawPacMan(pacManOpen,     posX);
-	            drawPacMan(pacManMidOpen,  posX);
+                drawGhost(ghostOne,        posX);	            
+                //delay(125);
+                drawPacMan(pacManMidOpen,  posX);
+                drawGhost(ghostTwo,        posX);                
+                //delay(125);
             }
         }
 
@@ -85,10 +141,25 @@ class AnimationLaguerre : public Animation {
 	        for (int i = 0 ; i < 13 ; i++){
 			    for (int j = 0 ; j < 13 ; j++){
 				    PIXELS[i+1][j+posX] = rgb(tab[i][j]*255, tab[i][j]*255, 0);
-				    //delay(250);
 			    }
 	        }
         }
+        
+        void drawGhost(int tab[13][14], int posX){
+            for (int i = 0 ; i < 13 ; i++){
+			    for (int j = 0 ; j < 14 ; j++){
+                    switch(tab[i][j]){
+                        case 1 : PIXELS[i+1][j+posX-14-2] = rgb(255, 0, 0);
+                                 break;
+                        case 2 : PIXELS[i+1][j+posX-14-2] = rgb(255, 255, 255);
+                                 break;
+                        case 3 : PIXELS[i+1][j+posX-14-2] = rgb(0, 0, 255);
+                                 break;
+                    }
+			    }
+	        }
+        }    
+
 
         // J'ai le droit d'ajouter des méthodes privées ici pour
         //   découper correctement mon code.
