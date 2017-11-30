dd7ba72178f7bf2bda63d8846b3a17ca62689c46
clovis-ai <clovis.onplag@gmail.com>
2017-11-30 18:24:41 +0100

    Push en l'état

--- examples/sequencer/sequencer.ino
+++ examples/sequencer/sequencer.ino
@@ -10,14 +10,8 @@
 
 #include "Adafruit_WS2801.h"
 #include "SPI.h" // Comment out this line if using Trinket or Gemma
-#ifdef __AVR_ATtiny85__
-#include <avr/power.h>
-#endif
 
-uint8_t dataPin  = 2;    // Yellow wire on Adafruit Pixels
-uint8_t clockPin = 3;    // Green wire on Adafruit Pixels
-
-Adafruit_WS2801 strip = Adafruit_WS2801(20, dataPin, clockPin);
+Adafruit_WS2801 strip = Adafruit_WS2801(300);
 
 /************* Variables globales 
   Attention, ces variables sont globales à tout le microcontrôleur, si les noms sont triviaux il
@@ -33,19 +27,15 @@ char*** PIXELS;    // Les couleurs RGB ou HSV de chaque pixel (attention : mémo
 #include <animations/animation_bands/animation_bands.cpp>
 #include <animations/animation_laguerre/animation_laguerre.cpp>
 
-const int NBRE_ANIMATIONS = 2;
+const int NBRE_ANIMATIONS = 1;
 
 Animation *Animations[NBRE_ANIMATIONS];
 int courrante;
 int tours;
 
-void reset(char*** tab){
-  for(int i = 0; i < WIDTH; i++){
-    for(int j = 0; j < HEIGHT; j++){
-      for(int c = 0; c < 3; c++){
-        tab[i][j][c] = 255;
-      }
-    }
+void reset(){
+  for(int b=0; b<WIDTH*HEIGHT*3; ++b){
+      ((char*)(PIXELS))[b] = 0;
   }
 }
 
@@ -58,21 +48,29 @@ void setup() {
     // TODO : allouer la mémoire des pixels, instancier la bilbiothèque Adafruit, déclarer les animations, appeler leur setup ...
     
     // Aide à la prise en main des pointeurs ci-dessous, code à supprimer, mettre dans un tableau de pointeurs sur des animations, etc
-    PIXELS = (char***)malloc(20*15*3);
-    reset(PIXELS);
-    AnimationBands *animation_bands =  new AnimationBands();
+    PIXELS = (char***)malloc(20*15*3*sizeof(char));
+    //reset(PIXELS);
+    /*AnimationBands *animation_bands =  new AnimationBands();
     Animations[NBAPP++] = animation_bands;
     AnimationLaguerre *animation_laguerre = new AnimationLaguerre();
     Animations[NBAPP++] = animation_laguerre;
     courrante = 0;
     tours = Animations[0]->setup(15,20,100);
+    strip.begin();*/
+    while(!Serial);
+
+    Serial.begin(9600);;
     strip.begin();
-    Serial.begin(9600);
+    Serial.print("BEGIN");
+
+    reset();
+    Serial.print("END RESET");
+
 }
 
 
 void loop() {
-    if(tours < 0){ // Donner le tour à la prochaine animation
+    /*if(tours < 0){ // Donner le tour à la prochaine animation
         reset(PIXELS);
         ++courrante;
         if(courrante >= NBRE_ANIMATIONS){
@@ -85,18 +83,20 @@ void loop() {
     // TODO : appeler le loop de l'animation courante, ou bien changer d'animation
     Animations[courrante]->loop();
     --tours;
-    
-    int i;
-    for(int x = 0; x < 15; ++x){
-        for(int y = 0; y < 20; ++y){
-            i = x * 15 + y;
-            Serial.print(Color((byte)PIXELS[x][y][0], (byte)PIXELS[x][y][1], (byte)PIXELS[x][y][2]));
-            Serial.print(" ");
-            strip.setPixelColor(i, Color((byte)PIXELS[x][y][0], (byte)PIXELS[x][y][1], (byte)PIXELS[x][y][2]));
+    */
+    //int i = 0;
+    for(int h=0; h<HEIGHT; ++h) {
+          for(int w=0; w<WIDTH; ++w) {
+            for(int c=0; c<3; ++c) {
+            i = i+3; //h * WIDTH + w;
+            //Serial.print(Color((byte)PIXELS[x][y][0], (byte)PIXELS[x][y][1], (byte)PIXELS[x][y][2]));
+            //Serial.print(" ");
+            //strip.setPixelColor(i, Color((byte)PIXELS[h][w][0], (byte)PIXELS[h][w][1], (byte)PIXELS[h][w][2]));
+            strip.setPixelColor(i, Color(0,20,0));
+            }
         }
-        Serial.println();
     }
-    Serial.println("\n");
+    //Serial.println("\n");
     strip.show();
     delay(100);
 }
