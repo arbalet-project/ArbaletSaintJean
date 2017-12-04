f4ce842f69bc8f3b225fcb3731300dff90e7243d
Dori4ne <dory.laguerre@gmail.com>
2017-12-04 22:12:52 +0100

    FEAT : ajout d'une animation rainbow

---
+++ animations/animation_rainbow/README.md
@@ -0,0 +1,12 @@
+# Animation Rainbow
+
+## A quoi ressemble mon animation
+Un arc en ciel qui défile
+
+## Comment fonctionne son code
+on calcul le hsv sur une echelle qui va de 0 a width+height
+on dessine ensuite cette couleur sur une digonale
+et on recommence jusqu'a arriver a width + height
+
+## Comment on pourra interagir avec
+setup pour initialiser, puis loop() pour dessiner une frame
---
+++ animations/animation_rainbow/animation_rainbow.cpp
@@ -0,0 +1,56 @@
+#include <Animation.h>
+
+#ifndef ANIMATION_RAINBOW
+#define ANIMATION_RAINBOW
+
+using namespace std;
+class AnimationRainbow : public Animation {
+
+    public: 
+
+        AnimationRainbow() {
+            delta = 0;
+        }
+
+        int setup(int height, int width, int loops) {
+            this->height = height;
+            this->width = width;
+	    }
+
+        void loop() {
+            int coordX = 0;
+            int coordY = 0;
+
+            for(int i= 0 ; i<width + height ; i++){
+                coordX = i;
+                coordY = 0;
+                int hue = (coordX * 1.0 / width) + delta;
+                if(hue > 1){
+                    hue--;
+                }
+                char* color = hsv(hue, 1, 1);
+
+                for(int j = 0; j<i+1; j++){
+                    if(coordY < height && coordX < width){
+                        PIXELS[coordY][coordX] = color;
+                    }
+                    coordX--;
+                    coordY++;
+                }
+            }
+            delta = delta + (1.0 / width);
+            if(delta > 1){
+                delta--;
+            }
+        }
+
+
+    private:
+
+        int height;
+        int width;
+        int delta;
+
+};
+
+#endif
